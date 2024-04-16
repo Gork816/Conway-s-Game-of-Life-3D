4f074b10306b60be12f9f1a6cd82fa980690466d
Gork <grishkakochetkov@gmail.com>
2024-04-17 00:00:30 +0700

    Created evolution script
    It probably works. I hope so.

---
+++ Assets/Scripts/Evolution.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evolution : MonoBehaviour
+{
+    int[,,] status = new int[30, 30, 30];
+    int[,,] neighbours = new int[30, 30, 30];
+    GameObject[,,] cells = new GameObject[30, 30, 30];
+
+    HashSet<Vector3Int> alive = new HashSet<Vector3Int>();
+    //active cells = alive cells + their neighbours
+    HashSet<Vector3Int> active = new HashSet<Vector3Int>();
+
+    [SerializeField]
+    GameObject cellObj;
+
+    private void CellStatusUpdate(Vector3Int cell)
+    {
+        int x = cell.x;
+        int y = cell.y;
+        int z = cell.z;
+
+        if ((neighbours[x, y, z] == 5) || (neighbours[x, y, z] == 4 && status[x, y, z] == 1))
+        {
+            status[x, y, z] = 1;
+            cells[x, y, z].SetActive(true);
+            alive.Add(cell);
+        } else
+        {
+            status[x, y, z] = 0;
+            cells[x, y, z].SetActive(false);
+        }
+    }
+
+    private void StatusUpdate()
+    {
+        foreach (var cell in active)
+            CellStatusUpdate(cell);
+        active.Clear();
+    }
+
+    private void CellAliveUpdate(Vector3Int cell)
+    {
+        int x = cell.x;
+        int y = cell.y;
+        int z = cell.z;
+        neighbours[x, y, z] -= 1;
+        //alive cell will interact with itself in the next loop
+
+        //interacting with all neighbours
+        for (int dx = -1; dx <= 1; dx++)
+            for (int dy = -1; dy <= 1; dy++)
+                for (int dz = -1; dz <= 1; dz++) {
+                    neighbours[(x + dx) % 30, (y + dy) % 30, (z + dz) % 30] += 1;
+                    active.Add(new Vector3Int(x + dx, y + dy, z + dz));
+                }
+    }
+
+    private void AliveUpdate()
+    {
+        foreach (var cell in alive)
+            CellAliveUpdate(cell);
+        alive.Clear();
+    }
+
+    public void Turn()
+    {
+        AliveUpdate();
+        StatusUpdate();
+
+        for (int x = 0; x < 30; x++)
+            for (int y = 0; y < 30; y++)
+                for (int z = 0; z < 30; z++)
+                    neighbours[x, y, z] = 0;
+    }
+
+    private void Start()
+    {
+        for (int x = 0; x < 30; x++)
+            for (int y = 0; y < 30; y++)
+                for (int z = 0; z < 30; z++)
+                {
+                    status[x, y, z] = 0;
+                    neighbours[x, y, z] = 0;
+                    cells[x, y, z] = Instantiate(cellObj, new Vector3(x, y, z), Quaternion.identity);
+                }
+    }
+}
